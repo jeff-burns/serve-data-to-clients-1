a0c8e37995e1a6da93a1be865e08a3e97f3a9f11
jeff-burns <1jeffreyburns@gmail.com>
2018-05-02 14:39:01 -0600

    make port dynamic

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
-const app = express()
+const PORT = process.env.PORT || 3000;
+// const app = express()
+const bodyParser  = require('body-parser')
+const morgan      = require('morgan')
+const cors        = require('cors')
+const app         = module.exports = express()
+// const port        = parseInt(process.env.PORT || 3000)
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Example app running'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(PORT, () => console.log('Example app listening on port 3000!'))
