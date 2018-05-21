f07ee30a7130cc90d8cd29cf0af89d61fc3a0051
jeff-burns <1jeffreyburns@gmail.com>
2018-05-21 10:07:39 -0600

    resubmit #2

--- index.js
+++ index.js
@@ -1,43 +1,41 @@
-const express = require('express')
+const express = require("express");
 const PORT = process.env.PORT || 3000;
 
-const bodyParser  = require('body-parser')
-const morgan      = require('morgan')
-const cors        = require('cors')
-const app         = express()
-const data        = require('./data/cohorts.json')
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+const data = require("./data/cohorts.json");
 
 function findById(data, id) {
-    for (let i = 0; i < data.length; i++) {
-        if (data[i].ID == id) {
-            return data[i];
-        }
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].id == id) {
+      return data[i];
     }
-    return null;
+  }
+  return null;
 }
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(morgan(process.env.NODE_ENV !== 'production' ? 'dev' : 'combined'))
-app.use(cors({origin: true, credentials: true}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan(process.env.NODE_ENV !== "production" ? "dev" : "combined"));
+app.use(cors({ origin: true, credentials: true }));
 
-app.get('/', (req, res) => {
-    res.json(data)
-})
-
-app.get('/:id', (req, res) => {
-    let record = findById(data, req.params.id)
-    if (!record){
-        res.status(404).json({
-            error: {
-                message: "No record found!"
-            }
-        });
-    } else {
-        res.json({data: record});
-    }
+app.get("/", (req, res) => {
+  res.json({data});
 });
 
-app.listen(PORT, () => console.log('Example app listening on port 3000!'))
+app.get("/:id", (req, res) => {
+  let record = findById(data, req.params.id);
+  if (!record) {
+    res.status(404).json({
+      error: {
+        message: "No record found!"
+      }
+    });
+  } else {
+    res.json({ data: record });
+  }
+});
 
-
+app.listen(PORT);
